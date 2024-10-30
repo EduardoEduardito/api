d7ae47cd69bf79a922ac11e682ee46c9e3e7de73
EduardoEduardito <eduardopereiradantas109@gmail.com>
2024-10-30 05:40:15 -0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
-# Exibe uma mensagem de início do processo de configuração
-echo "Iniciando a instalação do Chrome e do ChromeDriver..."
-
-# Cria o diretório para Chrome e ChromeDriver
+# Cria o diretório para o Chrome
 mkdir -p /opt/render/project/.render/chrome
 
-# Baixa o Google Chrome para Linux e instala no diretório personalizado
+# Baixa o Google Chrome para Linux
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O chrome.deb
 dpkg -x chrome.deb /opt/render/project/.render/chrome
 rm chrome.deb
 
-# Define a variável de versão do Chrome instalada para garantir compatibilidade com o ChromeDriver
-CHROME_VERSION=$( /opt/render/project/.render/chrome/opt/google/chrome/chrome --version | grep -oP "\d+\.\d+\.\d+")
-
-# Baixa o ChromeDriver correto compatível com a versão do Chrome instalada
-CHROMEDRIVER_VERSION=$(curl -sS "https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json" | grep -oP "${CHROME_VERSION}.\d+" | head -1)
-wget https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/${CHROMEDRIVER_VERSION}/linux64/chromedriver-linux64.zip -O chromedriver.zip
+# Baixa o ChromeDriver correto para Linux
+CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)
+wget https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_linux64.zip -O chromedriver.zip
 unzip chromedriver.zip -d /opt/render/project/.render/chrome/
 rm chromedriver.zip
+
+# Define permissões de execução para o ChromeDriver
 chmod +x /opt/render/project/.render/chrome/chromedriver
-
-# Instala as dependências do Python
-pip install -r requirements.txt
-
-echo "Configuração concluída com sucesso!"
